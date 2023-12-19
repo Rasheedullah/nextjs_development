69b059155ddb32a0dd71c9b94f46f4247c67f15e
Rasheedullah <54411206+Rasheedullah@users.noreply.github.com>
2023-12-19 18:15:44 +0500

    Add files via upload
    upload files

---
+++ next.config.js
@@ -0,0 +1,26 @@
+// const path = require('path')
+
+// module.exports = {
+//   trailingSlash: true,
+//   reactStrictMode: false,
+//   experimental: {
+//     esmExternals: false,
+//     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
+//   },
+//   webpack: config => {
+//     config.resolve.alias = {
+//       ...config.resolve.alias,
+//       apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+//     }
+
+//     return config
+//   }
+// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
